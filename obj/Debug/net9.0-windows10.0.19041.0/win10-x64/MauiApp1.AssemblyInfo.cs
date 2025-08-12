357a0ea3e430fa7f870bdf3f926adde46b7b1bc7
Ivan24-wq <iv8747583@gmail.com>
2025-08-12 14:08:02 +0300

    fixed bugs

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f124363b3ae7e5d288d577fa5a44517fbcda7e07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f52339b017d76012b5d1a57aaeddf0a060daf74a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
